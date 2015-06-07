265ca7a50944c41ed552c6cfa09a97d6d0378ed0
wj4y78308 <wj4y78308@gmail.com>
2015-06-07 12:42:23 +0800

    correct the shader

---
+++ Assets/Scripts/GUI_Button.cs
@@ -0,0 +1,224 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Leap;
+
+public class GUI_Button : MonoBehaviour {
+	//public Button[] opButton;
+
+	Menu menu;
+	Operations operations;
+
+
+	bool buttonTouchable = true;
+	//bool menuOpened = true;
+	float buttonDeltaTime = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		menu = GetComponent<Menu> ();
+		operations = GetComponent<Operations> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/*
+		for(int i = 0 ; i < 6 ; i++){
+			if (i != operations.opMode) {
+				menu.opButtons[i].image.color = Color.black;
+				menu.opButtons[i].GetComponentInChildren<Text> ().color = Color.white;
+			} else {
+				menu.opButtons[i].image.color = Color.white;
+				menu.opButtons[i].GetComponentInChildren<Text> ().color = Color.black;
+				
+			}
+		}
+		if(operations.opMode==0) operations.lineRenderer.material.color = Color.black;
+		else if(operations.opMode==1) operations.lineRenderer.material.color = Color.blue;
+*/
+
+	}
+	/*public Vector2 leapPositionToScreen(Vector leapVector)
+	{
+		return operations.cam.WorldToScreenPoint(leapVector.ToUnityTranslated());
+	}*/
+
+	public bool inActiveZoneFinger(Button button){
+		if (operations.hand.IsRight) {
+
+			RectTransform rectButton = button.GetComponent<RectTransform> (); 		
+			Rect box = new Rect (
+				Camera.main.WorldToScreenPoint (button.transform.position).x,
+				Camera.main.WorldToScreenPoint (button.transform.position).y,
+				//button.rect.x,
+				//button.rect.y,
+				//opButton.image.sprite.bounds.size.x,
+				//opButton.image.sprite.bounds.size.y
+				
+				rectButton.rect.width,
+				rectButton.rect.height
+				);   
+			//Vector3 point = Camera.main.WorldToScreenPoint (GameObject.Find ("SkeletalRightRobotHand(Clone)").transform.FindChild ("index").FindChild ("bone3").position);
+
+			GameObject finger;
+			if ((finger = GameObject.Find ("SkeletalRightRobotHand(Clone)/index/bone3")) != null){
+				
+				Vector3 point = Camera.main.WorldToScreenPoint(finger.transform.position);
+
+				//if(point != null){	
+
+					if (point.x <= box.x + box.width / 2 && point.y >= box.y + (-1 * (box.height)) / 2) {
+
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+
+
+	void OnGUI () {
+
+		operations.pointable = operations.frame.Pointables.Frontmost;
+		operations.touchZone = operations.pointable.TouchZone;
+		/*
+		if (operations.handsInFrame.Leftmost.GrabStrength == 1 && menuOpened == true) {//open/close menu
+			menuOpened = false;
+			if (menu.mainMenu.gameObject.activeSelf) {
+				menu.ShowMainMenu (false);
+			}else if (!menu.mainMenu.gameObject.activeSelf) { 
+				menu.ShowMainMenu (true);
+			}
+		} 
+		else if( operations.handsInFrame.Leftmost.GrabStrength <= 0.5 && menuOpened == false){
+			menuOpened = true;
+		}
+*/
+
+		if (buttonTouchable == true) {
+			switch (operations.touchZone) {
+			case Pointable.Zone.ZONE_NONE: // None
+				break;
+			case Pointable.Zone.ZONE_HOVERING: // Hovering
+		
+
+				break;
+			case Pointable.Zone.ZONE_TOUCHING: // Touching
+
+				for( int i = 0 ; i < 6 ; i++){ //operation
+					if (inActiveZoneFinger (menu.opButtons[i])) {	
+						if( menu.sliderPanel.gameObject.activeSelf){
+							operations.SetThickness();
+
+						}
+						buttonTouchable = false;
+						menu.ChangeOpMode(i);
+						menu.HideSlider ();
+					
+
+						return;
+					}
+				}
+				for(int i = 0 ; i < 3; i++){ //camera
+					if(inActiveZoneFinger (menu.camposButtons[i])){
+						if( menu.sliderPanel.gameObject.activeSelf)
+							operations.SetThickness();
+						buttonTouchable = false;
+						operations.SetCamPos(i);
+						menu.HideSlider ();
+
+						return;
+					}
+				}
+				for(int i = 0 ; i < 3; i++){ //visual state
+					if(inActiveZoneFinger (menu.visualButtons[i])){
+						if( menu.sliderPanel.gameObject.activeSelf)
+							operations.SetThickness();
+						buttonTouchable = false;
+						menu.ChangeVisualState(i);
+						menu.HideSlider ();
+
+						return;
+					}
+				}
+
+				if(inActiveZoneFinger (menu.loadButton)){
+					if( menu.sliderPanel.gameObject.activeSelf)
+						operations.SetThickness();
+				   menu.ShowLoadMenu(true);
+					buttonTouchable = false;
+					menu.HideSlider ();
+
+				}
+				else if(inActiveZoneFinger (menu.saveButton)){
+					if( menu.sliderPanel.gameObject.activeSelf)
+						operations.SetThickness();
+				   menu.ShowSaveDialog(true);
+					buttonTouchable = false;
+					menu.HideSlider ();
+
+				}
+				else if(inActiveZoneFinger(menu.undoButton)){
+					if( menu.sliderPanel.gameObject.activeSelf)
+						operations.SetThickness();
+					print ("undo");
+					operations.Undo();
+					buttonTouchable = false;
+					menu.HideSlider ();
+				
+				}
+				else if(inActiveZoneFinger(menu.redoButton)){
+					if( menu.sliderPanel.gameObject.activeSelf)
+						operations.SetThickness();
+					print("redo");
+					operations.Redo();
+					buttonTouchable = false;
+					menu.HideSlider ();
+
+				}
+				else if(inActiveZoneFinger(menu.cleanButton)){
+					if( menu.sliderPanel.gameObject.activeSelf)
+						operations.SetThickness();
+					operations.Clean();
+					print ("clean");
+					buttonTouchable = false;
+					menu.HideSlider ();
+
+				}
+				if( menu.sliderPanel.gameObject.activeSelf){
+				
+					if(inActiveZoneFinger(menu.yesButton))	{
+						operations.SetThickness();
+						print ("yes");
+					}
+				}
+				break;
+			}
+		}
+		else {
+			buttonDeltaTime -= Time.deltaTime;
+			if(buttonDeltaTime <= 0){
+				buttonTouchable = true;
+				buttonDeltaTime = 1.0f;
+			}
+
+	
+		}
+	}
+	/*
+	Vector2 ToVector2 (Vector leap){
+		leap = operations.frame.InteractionBox.NormalizePoint (leap);
+		return new Vector2 (UnityEngine.Screen.width*leap.x,UnityEngine.Screen.height*leap.y);
+
+	}*/
+
+
+
+	public Vector3 ToVector3 (Vector leap){
+		return new Vector3 (leap.x,leap.y,-leap.z);
+		
+	}
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,287 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Menu : MonoBehaviour {
+
+	public Button[] opButtons;
+	public Button[] visualButtons;
+	public Button[] camposButtons;
+
+	public Button redoButton;
+	public Button undoButton;
+	public Button cleanButton;
+	public Button yesButton;
+
+	//public GameObject opPanel;
+	//public GameObject leftPanel;
+
+	public Slider slider;
+	public Text sliderText;
+	public GameObject sliderPanel;
+	public Image colorPicker;
+	public Image colorRectsBG;
+	public Image paper;
+	public GameObject mainMenu;
+	public GameObject loadMenu;
+	public Button loadButton;
+	public Button saveButton;
+	public GameObject about;
+	public Button switchL;
+	public Button switchR;
+	public GameObject saveDialog;
+	public Text inputName;
+	public GameObject deleteDialog;
+	public Text modelName;
+
+	Operations operations;
+	Color pickedColor;
+	int hueColorNum = 20;
+	int brightnessNum = 4;
+	int sizeOfView = (int)Screen.height/32;
+	int borderSize = (int)Screen.height/250;
+	Rect[,] rectArray = new Rect[20, 4];
+	GUIStyle[,] styleArray = new GUIStyle[20,4];
+	Rect pickedColorRect;
+	Texture2D pickedColorTex;
+	bool showColorRects = true;
+	int prevWidth=Screen.width, prevHeight=Screen.height;
+	int currLoadScreen;
+
+	// Use this for initialization
+	void Start () {
+		operations = GetComponent<Operations> ();
+		SetUpColorArray ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Screen.width != prevWidth || Screen.height != prevHeight) {
+			SetUpColorArray();
+			operations.SetUpTexture();
+			prevWidth=Screen.width;
+			prevHeight=Screen.height;
+		}
+	}
+
+	void OnGUI () {
+		if(showColorRects){
+			for(int i = 0; i < hueColorNum; ++i) {
+				for(int j = 0; j < brightnessNum; ++j) {
+					if( GUI.Button(rectArray[i,j], GUIContent.none, styleArray[i,j]) ){
+						ChangeColor( styleArray[i,j].normal.background.GetPixel(0,0) );
+					}
+				}
+				GUI.DrawTexture(pickedColorRect, pickedColorTex);
+			}
+		}
+	}
+	/*public void HidePanel(GameObject m){
+		m.gameObject.SetActive (false);
+	}
+	public void ShowPanel(GameObject m){
+		m.gameObject.SetActive (true);
+	}
+*/
+	public void ChangeOpMode (int mode){
+		for (int i=0; i<6; i++) {
+			if( i==mode ){
+				opButtons[i].image.color = Color.white;
+				opButtons[i].GetComponentInChildren<Text>().color = Color.black;
+			}
+			else{
+				opButtons[i].image.color = Color.black;
+				opButtons[i].GetComponentInChildren<Text>().color = Color.white;
+			}
+		}
+		if(mode==0) operations.lineRenderer.material.color = Color.black;
+		else if(mode==1) operations.lineRenderer.material.color = Color.blue;
+		operations.opMode = mode;
+	}
+
+	public void ShowMainMenu (bool show) {
+		if (!show) {
+			mainMenu.gameObject.SetActive(false);
+			showColorRects = false;
+		}
+		else {
+			mainMenu.gameObject.SetActive(true);
+			if(!colorPicker.gameObject.activeSelf){
+				showColorRects = true;
+			}
+		}
+	}
+
+	public void ShowLoadMenu (bool show) {
+		if (show) {
+			if (operations.saveList.Count == 0) return;
+			ShowMainMenu (false);
+			operations.startCube.SetActive(false);
+			if (visualButtons[0].image.color == Color.white) operations.grid.SetActive (false);
+			foreach (GameObject co in operations.cubeObjects)
+				co.SetActive (false);
+			currLoadScreen = 0;
+			SwitchLoadScreen(0);
+			loadMenu.gameObject.SetActive (true);
+			operations.SetCamPos(1);
+			operations.isLoading = true;
+		}
+		else {
+			ShowMainMenu (true);
+			foreach (CubeMesh cm in FindObjectsOfType<CubeMesh> ())
+				Destroy(cm.gameObject);
+			foreach (GameObject co in operations.cubeObjects)
+				co.SetActive (true);
+			if (operations.cubeObjects.Count==0) operations.startCube.SetActive(true);
+			if (visualButtons[0].image.color == Color.white) operations.grid.SetActive (true);
+			loadMenu.gameObject.SetActive (false);
+			operations.SetCamPos(1);
+			operations.isLoading = false;
+		}
+	}
+
+	public void ShowSaveButton (bool show) {
+		saveButton.gameObject.SetActive (show);
+	}
+
+	public void SwitchLoadScreen (int num) {
+		currLoadScreen += (currLoadScreen + num < 0 || currLoadScreen + num >= operations.saveList.Count)? 0:num;
+		switchL.gameObject.SetActive(currLoadScreen > 0);
+		switchR.gameObject.SetActive(currLoadScreen < operations.saveList.Count-1);
+		foreach (CubeMesh cubeMesh in FindObjectsOfType<CubeMesh> ())
+			Destroy (cubeMesh.gameObject);
+		if (operations.saveList.Count == 0) {
+			ShowLoadMenu (false);
+			return;
+		}
+
+		SaveData saveData = operations.saveList [currLoadScreen];
+		List<CubeMesh> cubeMeshes = new List<CubeMesh> ();
+		foreach (MeshData meshData in saveData.meshList) {
+			CubeMesh cm = ((GameObject)Instantiate(operations.cubeObject, meshData.position.ToVector3(), Quaternion.identity)).GetComponent<CubeMesh> ();
+			cm.LoadFromMeshData(meshData);
+			cubeMeshes.Add(cm);
+		}
+		for (int i=0; i<saveData.meshList.Count; i++) {
+			if (saveData.meshList[i].symmetry>=0) cubeMeshes[i].symmetry = cubeMeshes[saveData.meshList[i].symmetry];
+		}
+		modelName.text = saveData.name;
+	}
+
+	public void ChangeVisualState (int num){
+		if (visualButtons[num].image.color == Color.black) {
+			visualButtons[num].image.color = Color.white;
+			visualButtons[num].GetComponentInChildren<Text>().color = Color.black;
+			if (num==0) operations.grid.SetActive (true);
+		}
+		else{
+			visualButtons[num].image.color = Color.black;
+			visualButtons[num].GetComponentInChildren<Text>().color = Color.white;
+			if (num==0) operations.grid.SetActive (false);
+		}
+	}
+
+	public void SetUpSlider (int value, int maxValue) {
+		slider.value = value;
+		slider.maxValue = maxValue;
+		SetSliderText ();
+		sliderPanel.gameObject.SetActive (true);
+		loadButton.gameObject.SetActive (false);
+		saveButton.gameObject.SetActive (false);
+		int mode = operations.opMode;
+		for (int i=0; i<6; i++)
+			//if (i != mode && i != 4)
+			if (i != mode)
+				opButtons [i].interactable = false;
+	}
+
+	public void SetSliderText (){
+		sliderText.text = slider.value.ToString ();
+	}
+
+	public void HideSlider () {
+		sliderPanel.gameObject.SetActive (false);
+		loadButton.gameObject.SetActive (true);
+		for (int i=0; i<6; i++)
+				opButtons [i].interactable = true;
+	}
+
+	public void PickColorFromPalette (){
+		ChangeColor (colorPicker.sprite.texture.GetPixel ( (int)(Input.mousePosition.x/Screen.width*colorPicker.sprite.texture.width)
+		                                                  , (int)(Input.mousePosition.y/colorPicker.canvas.scaleFactor)));
+	}
+
+	public void ChangePalette (){
+		showColorRects = !showColorRects;
+		colorPicker.gameObject.SetActive(!colorPicker.gameObject.activeSelf);
+		colorRectsBG.gameObject.SetActive(!colorRectsBG.gameObject.activeSelf);
+	}
+
+	public Color GetPickedColor () {
+		return pickedColor;
+	}
+
+	public void ShowAbout (bool show) {
+		about.SetActive (show);
+	}
+
+	public void ShowSaveDialog (bool show) {
+		inputName.text = "";
+		saveDialog.SetActive (show);
+	}
+
+	public void ShowDeleteDialog (bool show) {
+		deleteDialog.SetActive (show);
+	}
+
+	public int GetCurrLoadScreen () {
+		return currLoadScreen;
+	}
+
+	void ChangeColor (Color color){
+		pickedColor = color;
+		pickedColorTex.SetPixel(0, 0, pickedColor);
+		pickedColorTex.Apply();
+		operations.startCube.GetComponent<Renderer>().material.color = pickedColor;
+	}
+
+	void SetUpColorArray (){
+		Color color = new Color();
+		for(int i = 0; i < hueColorNum; ++i) {
+			for(int j = 0; j < brightnessNum; ++j) {
+				if(i == hueColorNum - 1) {
+					if(j == 3)
+						color = CV.HSVToRGB((float)i/hueColorNum,0.0f,0.25f);
+					else if(j == 2)
+						color = CV.HSVToRGB((float)i/hueColorNum,0.0f,0.5f);
+					else if(j == 1)
+						color = CV.HSVToRGB((float)i/hueColorNum,0.0f,0.75f);
+					else if(j == 0)
+						color = CV.HSVToRGB((float)i/hueColorNum,0.0f,1.0f);
+				} else {
+					if(j == 3)
+						color = CV.HSVToRGB((float)i/hueColorNum,1.0f,0.4f);
+					else if(j == 2)
+						color = CV.HSVToRGB((float)i/hueColorNum,1.0f,0.8f);
+					else if(j == 1)
+						color = CV.HSVToRGB((float)i/hueColorNum,0.7f,1.0f);
+					else if(j == 0)
+						color = CV.HSVToRGB((float)i/hueColorNum,0.5f,1.0f);
+				}
+				rectArray[i,j] = new Rect(0.5f*(Screen.width - hueColorNum * sizeOfView - (hueColorNum - 1) * borderSize) + i * sizeOfView + (i-1) * borderSize,
+				                Screen.height - (brightnessNum - j) * sizeOfView - (brightnessNum - j - 1) * borderSize,
+				                sizeOfView, sizeOfView);
+				Texture2D texture = new Texture2D(1, 1);
+				texture.SetPixel(0, 0, color);
+				texture.Apply();
+				styleArray[i,j] = new GUIStyle();
+				styleArray[i,j].normal.background = texture;
+			}
+		}
+		pickedColorRect = new Rect(0.5f*(Screen.width - hueColorNum * sizeOfView - (hueColorNum - 1) * borderSize) + hueColorNum * sizeOfView + (hueColorNum-1) * borderSize,
+		                     Screen.height - sizeOfView * 2, sizeOfView * 2, sizeOfView * 2);
+		pickedColorTex = new Texture2D(1, 1);
+		ChangeColor (Color.red);
+	}
+}
