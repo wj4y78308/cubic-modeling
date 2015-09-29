97606d35efd958e9cc72d692a4e14db7724d4106
wj4y78308 <wj4y78308@gmail.com>
2015-09-29 18:59:40 +0800

    Updated the color slider by the visual setting

---
+++ Assets/Scripts/HoverMenuItems/HueItemVisualSettings.cs
@@ -0,0 +1,57 @@
+﻿using Hover.Cast.Custom;
+using Hover.Cast.Custom.Standard;
+using Hover.Cast.Display.Standard;
+using Hover.Cast.Items;
+using Hover.Common.Custom;
+using Hover.Common.Items;
+using Hover.Common.Items.Types;
+using UnityEngine;
+
+namespace Hover.Demo.CastCubes.Custom {
+
+	/*================================================================================================*/
+	public class HueItemVisualSettings : HovercastItemVisualSettings {
+
+		private ItemVisualSettingsStandard vDefaultSettings;
+		private ItemVisualSettingsStandard vHueSettings;
+		private ISliderItem vHueSlider;
+        protected Menu menu;
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////
+        /*--------------------------------------------------------------------------------------------*/
+        public void Awake() {
+			vHueSettings = new ItemVisualSettingsStandard();
+			
+			vHueSlider = (ISliderItem)gameObject.GetComponent<HovercastItem>().GetItem();
+			vHueSlider.OnValueChanged += HandleValueChanged;
+            menu = GameObject.Find("Scripts").GetComponent<Menu>();
+        }
+
+		/*--------------------------------------------------------------------------------------------*/
+		protected override IItemVisualSettings GetSettingsInner(IBaseItem pItem,
+												IItemVisualSettings pDefault, bool pFillWithDefault) {
+			vDefaultSettings = (ItemVisualSettingsStandard)pDefault;
+			HandleValueChanged(null);
+			return vHueSettings;
+		}
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		private void HandleValueChanged(ISelectableItem pItem) {
+           
+            Color col = menu.GetPickedColor();
+			Color colFade = col;
+			colFade.a = 0.25f;
+
+			vHueSettings.FillWith(vDefaultSettings, false);
+			vHueSettings.Renderer = typeof(UiItemSliderRenderer);
+			vHueSettings.SelectionColor = col;
+			vHueSettings.SliderTrackColor = colFade;
+			vHueSettings.SliderFillColor = colFade;
+		}
+
+	}
+
+}
